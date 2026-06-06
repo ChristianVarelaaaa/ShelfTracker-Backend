f3910c0a8201244f5940bdde1bfc98d66ac6a9bb
VarelaChristianJr.A. <christianvarelajr@gmail.com>
2026-06-06 11:38:44 +0800

    fix: update cross origin

--- src/main/java/com/ShelfTracker/api/controller/AuthController.java
+++ src/main/java/com/ShelfTracker/api/controller/AuthController.java
@@ -10,7 +10,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "https://app.netlify.com/projects/ephemeral-strudel-07d7dc")
 public class AuthController {
 
     @PostMapping("/login")
