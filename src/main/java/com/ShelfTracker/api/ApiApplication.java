06bdbf892ad15ba26e4df22ba74def1ea9510f47
VarelaChristianJr.A. <christianvarelajr@gmail.com>
2026-06-06 10:02:28 +0800

    sana gumana na

--- src/main/java/com/ShelfTracker/api/ApiApplication.java
+++ src/main/java/com/ShelfTracker/api/ApiApplication.java
@@ -2,11 +2,12 @@ package com.ShelfTracker.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class ApiApplication {
 
     public static void main(String[] args) {
@@ -19,7 +20,7 @@ public class ApiApplication {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://ephemeral-strudel-07d7dc.netlify.app") // Your Netlify URL
+                        .allowedOrigins("https://ephemeral-strudel-07d7dc.netlify.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
